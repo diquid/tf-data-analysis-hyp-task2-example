f698c6fcdb3f084a448abb342ebadcfcc2df788c
Dilyara Fatykhova <72702419+diquid@users.noreply.github.com>
2023-04-14 01:47:02 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,7 +6,8 @@ chat_id = 1234206085
 
 def solution(x: np.array, y: np.array) -> bool:
     alpha = 0.08
-    p_val = stats.cramervonmises_2samp(x, y).pvalue
+    test = stats.ks_2samp(x, y)
+    p_val = test[1]
     if p_val < alpha:
         return True
     return False
