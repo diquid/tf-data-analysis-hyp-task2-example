50a016da0b234b7b53a8a0527a78c51135da8b16
Dilyara Fatykhova <72702419+diquid@users.noreply.github.com>
2023-04-14 01:54:59 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,8 +6,7 @@ chat_id = 1234206085
 
 def solution(x: np.array, y: np.array) -> bool:
     alpha = 0.08
-    test = stats.ks_2samp(x, y)
-    p_val = test[1]
+    p_val = stats.anderson_ksamp([x, y]).pvalue
     if p_val < alpha:
         return True
     return False
